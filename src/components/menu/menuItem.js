888cd90b39daa4020e488c2db5c24a0cfd0c950a
smith109 <devon.smith017@gmail.com>
2024-11-24 14:17:57 -0600

    Add a constructor to create menu items

---
+++ src/components/menu/menuItem.js
@@ -0,0 +1,8 @@
+export default class MenuItem {
+  constructor(name, description, price, category) {
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.category = category;
+  }
+}
