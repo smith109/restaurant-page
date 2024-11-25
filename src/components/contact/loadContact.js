a2c9887eeaf914885f1296b824af7560711cab76
smith109 <devon.smith017@gmail.com>
2024-11-24 21:07:56 -0600

    Add a function to load the contact page component

---
+++ src/components/contact/loadContact.js
@@ -0,0 +1,77 @@
+import createElement from "../../createElement.js";
+
+function createFormInput(type, id, rows, cols) {
+  if (type === 'textarea') {
+    return createElement(
+      'textarea',
+      { type, id, for: id, rows, cols }
+    );
+  };
+
+  if (type === 'submit') {
+    return createElement(
+      'button',
+      { type },
+      'Send message'
+    );
+  };
+
+  return createElement(
+    'input',
+    { type, id, for: id }
+  );
+}
+
+function createFormRow(labelText, inputType, inputId, rows, cols) {
+  if (!labelText && inputType === 'submit') {
+    return createElement(
+      'div',
+      { class: 'form-row' },
+      createFormInput(inputType)
+    );
+  };
+
+  return createElement(
+    'div',
+    { class: 'form-row' },
+    createElement('label', { for: inputId }, labelText),
+    createFormInput(inputType, inputId, rows, cols),
+  );
+}
+
+export default function loadContact() {
+  const header = createElement(
+    'div',
+    { class: 'header' },
+    createElement('h1', {}, 'Contact Us'),
+    createElement('p', {}, "We'd love to hear from you!"),
+  );
+
+  const infoSection = createElement(
+    'div',
+    { class: 'info-section' },
+    createElement('h2', {}, 'Get in Touch'),
+    createElement('p', {}, 'Address: 123 Coffee Street, Bean City CO'),
+    createElement('p', {}, 'Email: hello@reveriecafe.com'),
+    createElement('p', {}, 'Phone: (123) 456-7890'),
+  );
+
+  const form = createElement(
+    'form',
+    { action: '#' },
+    createElement('h2', {}, 'Send Us a Message'),
+    createFormRow('Name', 'text', 'name'),
+    createFormRow('Email Address', 'email', 'email'),
+    createFormRow('Message', 'textarea', 'message', 5, 30),
+    createFormRow('', 'submit')
+  );
+
+  const contactSection = createElement(
+    'div',
+    { class: 'contact-section' },
+    infoSection,
+    form
+  );
+
+  document.querySelector('#content').append(header, contactSection);
+}
