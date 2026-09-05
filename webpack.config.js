54369b2f0e183586a0fad80ca96243a442edc5c2
smith109 <devon.smith017@gmail.com>
2026-09-05 00:14:01 -0500

    Add rule to handle imported fonts

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,10 @@ export default {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
 };
